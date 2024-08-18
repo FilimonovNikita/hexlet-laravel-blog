16778ce846c7b9387a7f39dcdc1a4cdffcc24b7a
Nikita Filimonov <filimonovnikitav@gmail.com>
2024-08-18 18:15:11 +0300

    step 10

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,8 +8,28 @@
         <meta name="csrf-param" content="_token" />
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('js/app.js') }}"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
     </head>
     <body>
+        <nav class="navbar navbar-expand-sm navbar-light bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Hexlet Blog</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+                    aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarID">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" 
+                        href="{{ route('about.index') }}">About</a>
+                    </div>
+                    <div class="nav-link active" aria-current="page">
+                        <a href="{{ route('articles.index') }}">Articles</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
         <div class="container mt-4">
             <h1>@yield('header')</h1>
             <div>
@@ -17,5 +37,7 @@
                 тест
             </div>
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('about', [PageController::class, 'about'])
+  ->name('about.index');
+Route::get('articles', [ArticleController::class, 'index'])
+  ->name('articles.index');
+Route::get('articles/{id}', [ArticleController::class, 'show'])
+  ->name('articles.show');
